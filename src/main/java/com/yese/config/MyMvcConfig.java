d89159a72b0ccba2d6df54429d0912dbf5916425
yese <1321810135@qq.com>
2020-01-19 22:41:30 +0800

    Update MyMvcConfig.java

--- src/main/java/com/yese/config/MyMvcConfig.java
+++ src/main/java/com/yese/config/MyMvcConfig.java
@@ -12,7 +12,7 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 @Configuration
 public class MyMvcConfig implements WebMvcConfigurer {
 
-    //定制首页
+    // 定制首页
     @Override
     public void addViewControllers(ViewControllerRegistry registry) {
         registry.addViewController("/").setViewName("login");//输入/,跳login.html
@@ -28,7 +28,7 @@ public class MyMvcConfig implements WebMvcConfigurer {
     //注册拦截器
     @Override
     public void addInterceptors(InterceptorRegistry registry) {
-        //注册拦截器，及拦截请求和要剔除哪些请求!
+        // 注册拦截器，及拦截请求和要剔除哪些请求!
         //我们还需要过滤静态资源文件，否则样式显示不出来
         registry.addInterceptor(new LoginHandlerInterceptor())
                 .addPathPatterns("/**").excludePathPatterns("/index.html", "/", "/user/login", "/asserts/**", "/webjars/**");
