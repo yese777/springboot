1627b10f56a32d61544c42761382ecef322d6363
yese <1321810135@qq.com>
2020-01-19 22:43:28 +0800

    Update MyRedisConfig.java

--- src/main/java/com/yese/config/MyRedisConfig.java
+++ src/main/java/com/yese/config/MyRedisConfig.java
@@ -30,7 +30,7 @@ public class MyRedisConfig {
         jacksonSeial.setObjectMapper(om);
         // 值采用json序列化
         template.setValueSerializer(jacksonSeial);
-        //使用StringRedisSerializer来序列化和反序列化redis的key值
+        // 使用StringRedisSerializer来序列化和反序列化redis的key值
         template.setKeySerializer(new StringRedisSerializer());
         // 设置hash key 和value序列化模式
         template.setHashKeySerializer(new StringRedisSerializer());
