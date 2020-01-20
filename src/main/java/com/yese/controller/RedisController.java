c063907b4a01a6a0eb2b8c8eaa962d2d580bd5b0
yese <1321810135@qq.com>
2020-01-20 23:54:17 +0800

    修改

--- src/main/java/com/yese/controller/RedisController.java
+++ src/main/java/com/yese/controller/RedisController.java
@@ -43,6 +43,6 @@ public class RedisController {
             System.out.println("有");
             return o;
         }
-        
+
     }
 }
--- src/main/java/com/yese/mapper/DepartmentMapper.java
+++ src/main/java/com/yese/mapper/DepartmentMapper.java
@@ -9,4 +9,5 @@ import java.util.List;
 public interface DepartmentMapper {
 
     List<Department> getAll();
+    
 }
