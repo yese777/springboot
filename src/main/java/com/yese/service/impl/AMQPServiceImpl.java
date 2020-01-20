6b2c9915c970fc26f61a2491a7ed90c78b28b129
yese <1321810135@qq.com>
2020-01-20 23:56:14 +0800

    Update AMQPServiceImpl.java

--- src/main/java/com/yese/service/impl/AMQPServiceImpl.java
+++ src/main/java/com/yese/service/impl/AMQPServiceImpl.java
@@ -8,13 +8,13 @@ import org.springframework.stereotype.Service;
 @Service
 public class AMQPServiceImpl {
 
-    //监听的队列,可以是数组,监听队列lisi.dog，只要收到消息，立刻执行该方法,并清空队列
+    // 监听的队列,可以是数组,监听队列lisi.dog，只要收到消息，立刻执行该方法,并清空队列
     @RabbitListener(queues = "lisi.dog")
     public void receive01(User user) {
         System.out.println("接收到消息:" + user);
     }
 
-    //获取消息的所有信息
+    // 获取消息的所有信息
     @RabbitListener(queues = "zhangsan")
     public void receive02(Message message) {
         System.out.println("消息内容:" + message.getBody());
