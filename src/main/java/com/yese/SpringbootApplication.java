cd800d94080a612e6a9160a85ab908fa94d2ee43
张庆福 <15195988693@163.com>
2020-03-10 20:45:18 +0800

    修改

--- src/main/java/com/yese/SpringbootApplication.java
+++ src/main/java/com/yese/SpringbootApplication.java
@@ -11,6 +11,10 @@ import org.springframework.scheduling.annotation.EnableScheduling;
 @EnableAsync//开启异步任务注解
 @EnableRabbit//开启基于注解的RabbitMQ
 @MapperScan("com.yese.mapper")//扫描mapper包
+/**
+ *  @SpringBootApplication :用来标注一个主配置类，说明这是一个Spring Boot应用
+ *  这个类只能和 controller,service,dao 这些包同级,约定大于配置!
+ */
 @SpringBootApplication
 public class SpringbootApplication {
 
