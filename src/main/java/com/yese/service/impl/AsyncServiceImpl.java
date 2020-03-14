f59523d4c0b88459ed587f6a0e209449e15cddd8
张庆福 <15195988693@163.com>
2020-03-14 19:00:11 +0800

    修改

--- src/main/java/com/yese/service/impl/AsyncServiceImpl.java
+++ src/main/java/com/yese/service/impl/AsyncServiceImpl.java
@@ -14,13 +14,13 @@ public class AsyncServiceImpl implements AsyncService {
     @Override
     @Async
     public void asyncTest() {
-        System.out.println("处理开始...");
+        System.out.println("处理数据开始...");
         try {
             // 此处模拟耗时的方法
             Thread.sleep(5000);
         } catch (InterruptedException e) {
             e.printStackTrace();
         }
-        System.out.println("处理结束...");
+        System.out.println("处理数据结束...");
     }
 }
